6f6831d39b97cf31f614d7191f6fcdeea7f89eb7
GiovanniG <giovanni.gnascimento@gmail.com>
2014-01-25 18:10:32 -0200

    Create 17calcularVendasLatasMetroQuadrado.py

---
+++ 17calcularVendasLatasMetroQuadrado.py
@@ -0,0 +1,11 @@
+#Venda de latas
+
+metros = int(input('Informe os metros quadrados da área a ser pintada: '))
+
+valLitros = (int(metros / 3))
+latas = ( int (valLitros / 18))
+valorFull = ( int (80.00 * latas))
+
+print (valLitros)
+print (latas)
+print (valorFull)
