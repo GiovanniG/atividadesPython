40fbf080cb7d4dab2afb5ba9c28a420adf2f1e5d
GiovanniG <giovanni.gnascimento@gmail.com>
2014-01-25 14:40:38 -0200

    Create somarDoisNumeros.py

---
+++ somarDoisNumeros.py
@@ -0,0 +1,7 @@
+print ('Soma de dois numeros\n')
+
+numA = int(input('Informe um valor inteiro: '))
+numB = int(input('Informe um outro valor inteiro: '))
+resultado = numA+numB
+
+print('\nA soma desses valores é:',resultado)
