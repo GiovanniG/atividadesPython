867de5b4396e2aa147773f94dc1d5ec096263649
GiovanniG <giovanni.gnascimento@gmail.com>
2014-01-25 14:51:40 -0200

    Create converteMetrosEmMilimetros.py

---
+++ converteMetrosEmMilimetros.py
@@ -0,0 +1,6 @@
+print ('Conversor de metros para milímetros\n')
+
+valMetros = float(input('Informe um valor em metros: '))
+valMili = valMetros * 1000
+
+print ('\nO valor informado convertido para milímetros é :',valMili,'mm')
